6155e2d8a6378bb1745943eb55c4e25873c01e46
Ogba Anthony <46971748+Jakoozi@users.noreply.github.com>
2021-09-25 19:17:54 -0700

    Update plans.js
    edited plans

--- src/helpers/plans.js
+++ src/helpers/plans.js
@@ -1,47 +1,47 @@
 export const plans = [
   {
     title: "Basic",
-    price: "50",
+    price: "50 - $499",
     btnTxt: "",
     type: "basic",
     advantages: [
       "Hashrate worth 10.7$ Safemoon",
       "Plan duration of 7 days",
       "ROI of 50%",
-      "Minimum withdrawal of $75 worth of Safemoon"
+//       "Minimum withdrawal of $75 worth of Safemoon"
     ]
   },
   {
     title: "Average",
-    price: "500",
+    price: "500 - 4,999",
     type: "average",
     advantages: [
       "Hash Rate worth $107.14 worth of Safemoon",
       "Plan duration of 7 days",
       "ROI of 50%",
-      "Minimum withdrawal of $750 worth of Safemoon"
+//       "Minimum withdrawal of $750 worth of Safemoon"
     ]
   },
   {
     title: "Standard Contract",
-    price: "5,000",
+    price: "5,000 - 49,999",
     type: "standard",
     advantages: [
       "Hashrate worth $1,071 worth of Safemoon",
       "Plan duration of 7 days",
       "ROI of 50%",
-      "Minimum withdrawal of $7,500 worth of Safemoon"
+//       "Minimum withdrawal of $7,500 worth of Safemoon"
     ]
   },
   {
     title: "Premium",
-    price: "50,000",
+    price: "50,000 - 99,999",
     type: "premium",
     advantages: [
       "Hash Rate of $7,143 worth of Safemoon",
       "Plan duration of 14 days",
       "Total return 200%($100,000)",
-      "Minimum withdrawal of $100,000 dollar worth of Safemoon."
+//       "Minimum withdrawal of $100,000 dollar worth of Safemoon."
     ]
   },
   {
@@ -53,7 +53,7 @@ export const plans = [
       "Unlimited duration(200% after 14 days)",
       "Plan duration of 1 day",
       "100% Asset Protection",
-      "Minimum withdrawal of $150,000 worth of Safemoon"
+//       "Minimum withdrawal of $150,000 worth of Safemoon"
     ]
   }
 ];
